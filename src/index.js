594feaa993d27ed91953fe15c60b10833a675b2d
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-09-29 13:26:59 -0700

    Added react-router.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
+import VisibleVisitList from './containers/VisibleVisitList.js'
 import { fetchVisits } from './actions/visits'
 import { fetchStatuses } from './actions/statuses'
 import { fetchMembers } from './actions/members'
@@ -17,13 +19,17 @@ const store = createStore(reducer, applyMiddleware(
   loggerMiddleware
 ))
 
+const routes = <Route component={App}>
+  <Route path="/" component={VisibleVisitList} />
+</Route>
+
 store.dispatch(fetchVisits())
 store.dispatch(fetchStatuses())
 store.dispatch(fetchMembers())
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>{routes}</Router>
   </Provider>,
   document.getElementById('root')
 )
