240a295d21f4aea765f3c184acc1ab2715a90983
Lukas Hambsch <lukas.hambsch@gmail.com>
2017-04-17 17:06:05 -0700

    Upgrade redux-logger & redux-saga.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { browserHistory } from 'react-router'
 import { routerMiddleware } from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
