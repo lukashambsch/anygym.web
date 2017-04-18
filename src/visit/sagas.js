efa919ab986a14556f7c3405b18660cd7ba3999d
Lukas Hambsch <lukas.hambsch@gmail.com>
2017-04-18 15:05:28 -0700

    Update saga declarations.

--- src/visit/sagas.js
+++ src/visit/sagas.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-import { takeLatest } from 'redux-saga'
-import { call, put } from 'redux-saga/effects'
+import { call, put, takeLatest } from 'redux-saga/effects'
 
 import { CREATE_VISIT, REQUEST_VISITS } from './actions'
 import { receiveVisits, createVisitSuccess } from './actions'
@@ -35,9 +34,9 @@ function* fetchVisits(action) {
 }
 
 export function* visitSaga() {
-  yield* takeLatest(REQUEST_VISITS, fetchVisits)
+  yield takeLatest(REQUEST_VISITS, fetchVisits)
 }
 
 export function* createVisitSaga() {
-  yield* takeLatest(CREATE_VISIT, createVisit)
+  yield takeLatest(CREATE_VISIT, createVisit)
 }
